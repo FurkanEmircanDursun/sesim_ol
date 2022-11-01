910c2aac197371aa689d48628a36e1db5d3967fc
dursunfurkann <furkandursunn18@gmail.com>
2022-11-01 13:57:32 +0300

    Initial commit

---
+++ lib/choice_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sesim_ol/logined_page.dart';
+
+class ChoicePage extends StatelessWidget {
+
+   var list=[Icons.fastfood_outlined,Icons.music_note_outlined,Icons.movie_outlined,Icons.videogame_asset_off,Icons.abc,Icons.safety_check,Icons.access_alarm,Icons.kayaking];
+  var colors=[Colors.green,Colors.red,Colors.grey,Colors.blueAccent,Colors.purple,Colors.black12,Colors.amberAccent,Colors.orangeAccent];
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+  appBar: AppBar(
+    backgroundColor: Colors.blueAccent,
+    centerTitle: true,
+    title: Text('Species',style: TextStyle(fontWeight: FontWeight.bold)),
+  ),
+      body: SafeArea(
+        child: GridView.count(
+
+        crossAxisCount: 2 ,
+        children: List.generate(8,(index){
+          return Container(
+            child: InkWell(
+              onTap: (){
+                if( index==0) {
+
+                  Navigator.pop(context);
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginedPage()));
+                }
+
+              },
+              child: Card(
+
+                child: Icon(
+                  list[index],
+                  size: 60,
+                  color: Colors.white,
+                ),
+                color: colors[index],
+              ),
+            ),
+          );
+        }),
+    ),
+      ),
+
+
+
+
+    );
+  }
+}
